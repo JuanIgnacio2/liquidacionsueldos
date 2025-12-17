db6c28788a80fa4e3db81769d49dccd50a1abdcd
JuanIgnacio2 <juanignacioleivachaves@gmail.com>
2025-12-17 15:52:29 -0300

    Agregado desplegable con lista de conceptos, correcciones en valores de liquidación

--- src/Components/EmployeeViewModal/EmployeeViewModal.jsx
+++ src/Components/EmployeeViewModal/EmployeeViewModal.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Modal } from '../Modal/Modal';
-import { User, DollarSign, Building, FileText, ListChecks, Edit } from 'lucide-react';
+import { User, DollarSign, Building, FileText, ListChecks } from 'lucide-react';
 import * as api from "../../services/empleadosAPI";
 
 // Función helper para formatear moneda en formato argentino ($100.000,00)
